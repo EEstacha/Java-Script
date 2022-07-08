b6df2c479715eb4e48bbc10f81ba1af249639ccf
EEstacha <106624595+EEstacha@users.noreply.github.com>
2022-07-08 21:15:31 +0200

    sistema de multas

---
+++ ex008/ex008.js
@@ -0,0 +1,15 @@
+var pais = 'Malawi'
+var vel = 80
+console.log(`Vivendo em ${pais},`)
+
+if(pais== 'Moçambique'){
+    console.log('Você é Moçambicano')
+}  else{
+    console.log('Você é Estrangeiro')
+}
+console.log(`A sua velocidade é de ${vel} km/h`)
+if(vel>60){
+    console.log(`Está multado por Condução com excesso de velocidade, obrigado`)
+} else{
+    console.log('Use sempre o cinto de seguraça, boa viagem')
+}
